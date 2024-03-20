ed2ba6aec8a9194ff3964799ac434b9e183a47eb
Bryan Joseph <bryanllana14@gmail.com>
2024-03-20 16:11:11 -0500

    Header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-teal-500 py-5">
+      <h1 className="text-center text-4xl font-bold">Calculadora de Propinas y Consumo</h1>
+    </header>
+  )
+}
+
+export default Header
